b45aed7fa4d30daba1420b07dd64acb7f50ec5d1
Derrick Furtado <derrick.furtado10@gmail.com>
2024-07-29 13:13:29 -0600

    made changes to format and added contact form

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("index.js connected")
+
+function validateForm() {
+    var name = document.getElementById('field').value;
+    var email = document.getElementById('email').value;
+    var company = document.getElementById('field').value;
+    var message = document.getElementById('field').value;
+
+    if (!name || !email || !company || !message) {
+        alert("All fields are required.");
+        return false;
+    }
+    return true;
+}
